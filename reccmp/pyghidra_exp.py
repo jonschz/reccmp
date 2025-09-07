782db53ba4a41adab0ff39e66fb9b475bac12cbd
jonschz <jonschz@users.noreply.github.com>
2025-09-07 13:20:54 +0200

    Cleanup

--- reccmp/pyghidra_exp.py
+++ reccmp/pyghidra_exp.py
@@ -1,31 +1,38 @@
-from typing import TYPE_CHECKING
-import pyghidra
-
 # from pyghidra import GuiPyGhidraLauncher
 from pyghidra import HeadlessPyGhidraLauncher
 
-if TYPE_CHECKING:
-    from ghidra.program.model.listing import Program
+# Suppress linter warnings related to the fact that the header support for Ghidra is limited
+# and that we cannot import Ghidra classes before Ghidra has been loaded
+
+# pylint: disable=import-outside-toplevel
+# pyright: reportMissingModuleSource=false
+
+
+# TODO
+# - Refactor to logging library
+# - Test again if anything GUI related is possible at all
+# - Load a remote project, check out and check in a remote project
+#   - possibly something like
+#     ```
+#     repo = GhidraProject.getServerRepository(...)
+#     repo.checkout(...)
+#     ```
+#   - set up local server for testing
+#   - search API for "checkin" -> multiple candidates
+
 
 def main():
-    print("Hi")
+    print("Starting Ghidra...")
 
     # launcher = GuiPyGhidraLauncher()
     launcher = HeadlessPyGhidraLauncher()
     launcher.start()
-    # TODO: Loading existing remote projects seems to be hard
-    # This opens the project with a new binary file, but it requires the binary file to be specified, which we don't have in a shared project with an existing file
-    # with pyghidra.open_program("../isle/legobin/LEGO1.DLL", analyze=False, project_name="isle", project_location="C:\\Users\\Jonathan\\Documents\\ghidra", program_name="pyghidra_test") as flat_api:
 
-    #     print("Loaded")
-    #     # This does not work, unlike the Ghidrathon script. Do we have the flat API actually flat?
-    #     # getCurrentProgram()
-    #     program: Program = flat_api.getCurrentProgram()
-    #     print(program.getName())
-
+    print("Ghidra started. Opening Ghidra project...")
 
     from ghidra.base.project import GhidraProject
     from ghidra.program.flatapi import FlatProgramAPI
+    from reccmp.ghidra_scripts.import_functions_and_types_from_pdb import main as reccmpImportMain
 
     # based on the source code of pyghidra.open_program()
     project = GhidraProject.openProject("C:\\Users\\Jonathan\\Documents\\ghidra", "isle", True)
@@ -33,22 +40,24 @@ def main():
     files = list(folder.getFiles())
     print(files)
 
+
+    print("Ghidra project opened. Opening Program...")
+
     program = project.openProgram("/", "LEGO1.DLL (Ghidra 11.4.2 experiment)", True)
     print(program)
     print(program.getName())
 
-    # Nice, this actually works! I could conceivably try to run the import script from here
+    print("Program opened. Starting reccmp import...")
 
-    from reccmp.ghidra_scripts.import_functions_and_types_from_pdb import main as importMain
-
-    # TODO: Ideas
-    # - How to get the FlatProgramApi? Is it the same as returned by project.openProgram?
-    # - give optional parameter to `main` with the API
-    # - store the flat program API in a global if exists (not very clean)
-
-    api = FlatProgramAPI(program)
-
-    importMain(api)
+    # This name is irrelevant, but we have to give it some name
+    transaction_name = "pyghidra-reccmp-import-script"
+    transaction = program.startTransaction(transaction_name)
+    try:
+        api = FlatProgramAPI(program)
+        reccmpImportMain(api)
+    finally:
+        # Second argument: Whether to commit (True) or abort (False)
+        program.endTransaction(transaction, True)
 
 
 if __name__ == "__main__":
