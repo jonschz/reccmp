e8c1678231134794f02039b8be88510df2c4f497
jonschz <jonschz@users.noreply.github.com>
2025-09-13 07:54:40 +0200

    WIP: save progress

--- reccmp/pyghidra_exp.py
+++ reccmp/pyghidra_exp.py
@@ -1,4 +1,5 @@
 # from pyghidra import GuiPyGhidraLauncher
+import time
 from pyghidra import HeadlessPyGhidraLauncher
 
 # Suppress linter warnings related to the fact that the header support for Ghidra is limited
@@ -32,6 +33,8 @@ def main():
 
     from ghidra.base.project import GhidraProject
     from ghidra.program.flatapi import FlatProgramAPI
+    from ghidra.util.task import TaskMonitor
+    from ghidra.framework.model import TransactionInfo
     from reccmp.ghidra_scripts.import_functions_and_types_from_pdb import main as reccmpImportMain
 
     # based on the source code of pyghidra.open_program()
@@ -43,7 +46,7 @@ def main():
 
     print("Ghidra project opened. Opening Program...")
 
-    program = project.openProgram("/", "LEGO1.DLL (Ghidra 11.4.2 experiment)", True)
+    program = project.openProgram("/", "LEGO1.DLL (Ghidra 11.4.2 experiment)", False)
     print(program)
     print(program.getName())
 
@@ -51,13 +54,33 @@ def main():
 
     # This name is irrelevant, but we have to give it some name
     transaction_name = "pyghidra-reccmp-import-script"
-    transaction = program.startTransaction(transaction_name)
-    try:
-        api = FlatProgramAPI(program)
-        reccmpImportMain(api)
-    finally:
+    # transaction = program.startTransaction(transaction_name)
+    # try:
+        # api = FlatProgramAPI(program)
+        # reccmpImportMain(api)
+    # finally:
         # Second argument: Whether to commit (True) or abort (False)
-        program.endTransaction(transaction, True)
+        # program.endTransaction(transaction, True)
+
+    # TODO: Try a local file first before trying to fix the online repo
+    # TODO: Look into the existing setup, maybe it does something else to address this
+
+    # WIP: experiments trying to finish a potential transaction. Might be completely wrong
+    while True:
+        transaction_info = program.getCurrentTransactionInfo()
+        status = transaction_info.getStatus()
+
+        if status != TransactionInfo.Status.NOT_DONE:
+            break
+
+        time.sleep(1.0)
+        print("Waiting for transaction...")
+        id = int(transaction_info.getID())
+        program.endTransaction(id, True)
+
+
+    # WIP: Saving fails with some transaction warning
+    program.save("Test file comment", TaskMonitor.DUMMY)
 
 
 if __name__ == "__main__":
