94d9dbd891fcd8818998be12cbd43cde560ef74b
jonschz <jonschz@users.noreply.github.com>
2025-09-13 10:02:38 +0200

    Fix saving the program

--- reccmp/pyghidra_exp.py
+++ reccmp/pyghidra_exp.py
@@ -1,5 +1,4 @@
 # from pyghidra import GuiPyGhidraLauncher
-import time
 from pyghidra import HeadlessPyGhidraLauncher
 
 # Suppress linter warnings related to the fact that the header support for Ghidra is limited
@@ -33,9 +32,8 @@ def main():
 
     from ghidra.base.project import GhidraProject
     from ghidra.program.flatapi import FlatProgramAPI
-    from ghidra.util.task import TaskMonitor
-    from ghidra.framework.model import TransactionInfo
     from reccmp.ghidra_scripts.import_functions_and_types_from_pdb import main as reccmpImportMain
+    from ghidra.app.script import GhidraScriptUtil
 
     # based on the source code of pyghidra.open_program()
     project = GhidraProject.openProject("C:\\Users\\Jonathan\\Documents\\ghidra", "isle", True)
@@ -47,40 +45,31 @@ def main():
     print("Ghidra project opened. Opening Program...")
 
     program = project.openProgram("/", "LEGO1.DLL (Ghidra 11.4.2 experiment)", False)
-    print(program)
-    print(program.getName())
 
     print("Program opened. Starting reccmp import...")
 
+    # Not exactly sure why this is necessary, but it can't hurt
+    GhidraScriptUtil.acquireBundleHostReference()
+
     # This name is irrelevant, but we have to give it some name
     transaction_name = "pyghidra-reccmp-import-script"
-    # transaction = program.startTransaction(transaction_name)
-    # try:
-        # api = FlatProgramAPI(program)
-        # reccmpImportMain(api)
-    # finally:
+    transaction = program.startTransaction(transaction_name)
+    try:
+        api = FlatProgramAPI(program)
+        reccmpImportMain(api)
+    finally:
         # Second argument: Whether to commit (True) or abort (False)
-        # program.endTransaction(transaction, True)
-
-    # TODO: Try a local file first before trying to fix the online repo
-    # TODO: Look into the existing setup, maybe it does something else to address this
-
-    # WIP: experiments trying to finish a potential transaction. Might be completely wrong
-    while True:
-        transaction_info = program.getCurrentTransactionInfo()
-        status = transaction_info.getStatus()
-
-        if status != TransactionInfo.Status.NOT_DONE:
-            break
-
-        time.sleep(1.0)
-        print("Waiting for transaction...")
-        id = int(transaction_info.getID())
-        program.endTransaction(id, True)
+        program.endTransaction(transaction, True)
 
 
-    # WIP: Saving fails with some transaction warning
-    program.save("Test file comment", TaskMonitor.DUMMY)
+    # Not exactly sure why this is necessary, but it can't hurt
+    GhidraScriptUtil.releaseBundleHostReference()
+
+    # Note that `program.save()` is wrong and does not work.
+    project.save(program)
+    project.close(program)
+
+    print("Done!")
 
 
 if __name__ == "__main__":
